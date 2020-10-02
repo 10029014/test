82843fb329dc9f4b258998358bd8c3474113c92d
10029014 <10029014@lcwc.ac.uk>
2020-10-02 09:35:58 +0000

    Created code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  /*local variable definition*/
+  int a, b;
+  int g;
+
+  /* actual initialization*/
+  a = 10;
+  b = 20;
+  g = a+b;
+
+  printf ("value of a = %d, b = %d and g = %d\n", a, b, g);
+
+  return 0;
+}
